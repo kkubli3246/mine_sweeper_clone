c96435a7bb057b6f72d33c2d81145dd01ea2036a
Summer Guijarro <summerguijarro@Summers-MacBook-Air.local>
2019-03-20 00:17:13 -0700

    Able to set Game difficulty, Panels react to hover, renders panels based on difficulty

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -7,16 +7,44 @@ class GameBoard extends Component{
         super(props);
         this.state ={
             difficulty: this.props.difficulty,
+            rows: [],
+            cols: []
             
         }
     }
-    populateBoard = (difficulty) =>{
-
+    componentWillReceiveProps = (nextProps) =>{
+        if(nextProps.difficulty !== this.props.difficulty){
+            this.setState({difficulty: nextProps.difficulty})
+        }
+    }
+    setBoardSize = () =>{
+        if(this.state.difficulty == 'easy'){
+            this.setState({rows: [0,1,2,3,4,5,6,7,8]});
+        }else if(this.state.difficulty == 'intermediate'){
+            this.setState({rows: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]});
+        }else if(this.state.difficulty == 'expert'){
+            this.setState({rows: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]});
+        }
     }
 
+    drawBoard = () => {
+       const currentBoard = this.state.rows.map((panels, i) =>
+        <div key = {i}><Panel /></div>
+       );
+       return currentBoard;
+       
+    }
     render(){
         return(
-            <Panel/>
+            <div>
+                
+                <h1>{this.state.difficulty}</h1>
+                <button onClick = {this.setBoardSize}>New Game</button>
+                <div>
+                    {this.drawBoard()}
+                </div>
+                
+            </div>
         )
     }
 }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Panel extends Component{
-
+    constructor(props){
+        super(props);
+        this.state ={
+            hover: 'panel'
+        }
+    }
+    handleMouseEnter = () =>{
+        this.setState({hover: 'panel-hover'});
+    }
+    handleMouseLeave = () =>{
+        this.setState({hover: 'panel'});
+    }
     render(){
         return(
-        <div id ='panel'>
+        <div id ={this.state.hover} onMouseEnter = {() => this.handleMouseEnter()} onMouseLeave ={() => this.handleMouseLeave()}>
             
         </div>
         )
